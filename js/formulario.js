f32f18e8ad78217ba2d848cec6e19e15d5cd4f4e
Rayjp <rainier86@gmail.com>
2019-10-23 11:24:25 -0430

    Formulario de email validado

--- js/formulario.js
+++ js/formulario.js
@@ -1,13 +1,29 @@
-$("#formulario").submit(function(event)
-{
-
-	event.preventDefault(); //Almacena los datos sin refrescar el sitio
-	enviar();
-
-
-
+//console.log("funcionando");
+$("#formulario").submit(function(event){
+    event.preventDefault(); //almacena los datos sin refrescar el sitio web.
+    enviar();
 });
-
 function enviar(){
-	console.log("ejecutado");
+    //console.log("ejecutado");
+    var datos = $("#formulario").serialize(); //toma los datos "name" y los lleva a un arreglo.
+    $.ajax({
+        type: "post",
+        url:"formulario.php",
+        data: datos,
+        success: function(texto){
+            if(texto=="exito"){
+                correcto();
+            }else{
+                phperror(texto);
+            }
+        }
+    })
+}
+function correcto(){
+    $("#mensajeExito").removeClass("d-none");
+    $("#mensajeError").addClass("d-none");
+}
+function phperror(texto){
+    $("#mensajeError").removeClass("d-none");
+    $("#mensajeError").html(texto);
 }
